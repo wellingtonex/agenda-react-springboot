b139b9cb20a8a4b5aa0c9917c69b687266f06c8e
wellingtonx <wellingtonex@gmail.com>
2019-08-19 19:59:39 -0300

    preparando para subir localmente com doker-compose

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getToken } from "./auth";
 
 const api = axios.create({
-  baseURL: 'http://ec2-54-90-119-170.compute-1.amazonaws.com:8080'
+  baseURL: 'http://localhost:8080'
 });
 
 api.interceptors.request.use(async config => {
