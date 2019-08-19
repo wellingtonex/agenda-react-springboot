66803774d14f079b694e05f3b13196bdb8dabb11
wellingtonx <wellingtonex@gmail.com>
2019-08-19 16:57:58 -0300

    renomeando diretorio

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getToken } from "./auth";
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080'
+  baseURL: 'http://servidor-agenda:8080'
 });
 
 api.interceptors.request.use(async config => {
